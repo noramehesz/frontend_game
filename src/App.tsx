ca56690c4627b8fa16e9ffa7afcfebeb8aac5543
noramehesz <noramehesz@gmail.com>
2020-08-03 22:34:47 +0200

    splash image

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import { Sprite, Stage } from "react-pixi-fiber";
+import splash from './images/splashexample.png';
+import * as PIXI from 'pixi.js';
+
+
+/*function useInterval(callback: () => {}, delay: number) {
+  const intervalRef = React.useRef(any);
+  const callbackRef = React.useRef(callback);
+
+  React.useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  React.useEffect(() => {
+      intervalRef.current = window.setInterval(() => callbackRef.current(), delay);
+      return () => window.clearInterval(intervalRef.current);
+  }, [delay]);
+  
+  return intervalRef;
+}*/
+
 
 function App() {
+  const [spalshImage, setSplashImage] = useState({alpha: 1, visible: true});
+
+  const requestRef = useRef(0);
+  const prevValueRef = useRef(1);
+
+  const animate = () => {
+    if (prevValueRef.current !== undefined) {
+      let newAlpha = prevValueRef.current - 0.01;
+      if (newAlpha <= 0) {
+        setSplashImage({
+          visible: false,
+          alpha: 0,
+        });
+        return 0;
+      }
+      setSplashImage({
+        ...spalshImage,
+        alpha: newAlpha,
+      });
+      prevValueRef.current = newAlpha; 
+      requestRef.current = requestAnimationFrame(animate);
+    }
+  }
+
+ /* const fadeOut = useCallback(() => {
+    function decrease() {
+      let newAlpha = spalshImage.alpha - 0.05;
+      console.log(newAlpha);
+      if (newAlpha <= 0) {
+        setSplashImage({
+          alpha: 0,
+          visible: false,
+        });
+        return true;
+      }
+      setSplashImage({
+        ...spalshImage,
+        alpha: newAlpha,
+      }); 
+      requestAnimationFrame(decrease);
+    }
+    decrease();
+  }, [spalshImage]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fadeOut();
+    }, 2000);
+  }, [fadeOut]);*/
+
+  useEffect(() => {
+    setTimeout(() => {
+      requestRef.current = requestAnimationFrame(animate);
+      const script = document.createElement('script');
+      script.src = './space.js';
+      script.async = true;
+      document.body.appendChild(script);
+      return () => {
+        cancelAnimationFrame(requestRef.current);
+        document.body.removeChild(script);  
+      }
+    }, 2000);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Stage options={{width: 800, height: 600,}}>
+      { spalshImage.visible &&
+          <Sprite texture={PIXI.Texture.from(splash)} alpha={spalshImage.alpha} zIndex={100}></Sprite>
+      }
+    </Stage>
+);
 }
 
 export default App;
---
+++ src/bgAnimation.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as PIXI from 'pixi.js';
+
+function Bganimation() {
+
+    return ()
+
+}
